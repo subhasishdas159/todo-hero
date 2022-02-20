d1b50b4732e9b97295f8cb39f38ba5de0ea331a8
Subhasish Das <subhasish@doc32.in>
2022-02-21 00:22:43 +0530

    delete functionality is okay

--- src/comp/Nav.svelte
+++ src/comp/Nav.svelte
@@ -20,22 +20,27 @@
 			>
 		</a>
 	</li>
-	<li>
-		<a>
+	<li
+		on:click={() => {
+			const notDone = $todos.filter((todoItem) => !todoItem.isDone);
+			$todos = notDone;
+		}}
+	>
+		<span>
 			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-5 w-5"
+				class="w-5 h-5"
 				fill="none"
-				viewBox="0 0 24 24"
 				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
 				><path
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					stroke-width="2"
-					d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+					d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
 				/></svg
 			>
-		</a>
+		</span>
 	</li>
 	<li
 		on:click={() => {
