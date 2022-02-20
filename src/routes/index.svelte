ba5aa9c083ca05dca30ef783a1205f5e4eda99ea
Subhasish Das <subhasish@doc32.in>
2022-02-20 18:13:33 +0530

    new branch

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,11 @@
 <div class="max-w-6xl mx-auto">
 	<div class="container mx-auto px-6">
-		<div class="flex flex-row gap-2">
+		<!-- <div class="flex flex-row gap-2"> -->
 			<input
 				type="text"
 				placeholder="Type here"
 				class="input input-bordered input-primary w-full max-w-xs"
 			/>
-		</div>
+		<!-- </div> -->
 	</div>
 </div>
