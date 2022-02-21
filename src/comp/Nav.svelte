f454858e4df015bd2e1b1a380fe75c211d3d87b2
Subhasish Das <subhasish@doc32.in>
2022-02-21 10:59:40 +0530

    mobile hover

--- src/comp/Nav.svelte
+++ src/comp/Nav.svelte
@@ -3,22 +3,29 @@
 </script>
 
 <ul class="menu bg-white menu-horizontal rounded-box shadow-lg border">
-	<li>
-		<a>
+	<li
+		on:click={() => {
+			const notDone = $todos.filter((todoItem) => !todoItem.isDone);
+			$todos = notDone;
+		}}
+	>
+		<span
+			class="mobileOnly:hover:bg-white mobileOnly:hover:text-neutral mobileOnly:focus:bg-blue-300 mobileOnly:active:bg-success mobileOnly:active:text-white"
+		>
 			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="h-5 w-5"
+				class="w-5 h-5"
 				fill="none"
-				viewBox="0 0 24 24"
 				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
 				><path
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					stroke-width="2"
-					d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+					d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
 				/></svg
 			>
-		</a>
+		</span>
 	</li>
 	<li
 		on:click={() => {
@@ -26,7 +33,9 @@
 			$todos = notDone;
 		}}
 	>
-		<span>
+		<span
+			class="mobileOnly:hover:bg-white mobileOnly:hover:text-neutral mobileOnly:focus:bg-blue-300 mobileOnly:active:bg-error mobileOnly:active:text-white"
+		>
 			<svg
 				class="w-5 h-5"
 				fill="none"
@@ -55,7 +64,9 @@
 			$todos = [...notDone, ...done];
 		}}
 	>
-		<span>
+		<span
+			class="mobileOnly:hover:bg-white mobileOnly:hover:text-neutral mobileOnly:focus:bg-blue-300 mobileOnly:active:bg-info mobileOnly:active:text-white"
+		>
 			<svg
 				class="w-5 h-5"
 				fill="none"
