5a3e6ef7c5d6e427f022cf531897b2d729b82e95
Subhasish Das <subhasish@doc32.in>
2022-02-22 14:45:30 +0530

    wip

--- src/comp/Nav.svelte
+++ src/comp/Nav.svelte
@@ -8,15 +8,22 @@
 >
 	<li
 		on:click={() => {
-			const notDone = $todos.filter((todoItem) => !todoItem.isDone);
-			$todos = notDone;
+			const hard = $todos.filter((todoItem) => todoItem.isHard);
+			const notHard = $todos.filter((todoItem) => !todoItem.isHard);
+			const important = [...hard, ...notHard].filter((todoItem) => todoItem.isImportant);
+			const notImportant = [...hard, ...notHard].filter((todoItem) => !todoItem.isImportant);
+			const urgent = [...important, ...notImportant].filter((todoItem) => todoItem.isUrgent);
+			const notUrgent = [...important, ...notImportant].filter((todoItem) => !todoItem.isUrgent);
+			const done = [...urgent, ...notUrgent].filter((todoItem) => todoItem.isDone);
+			const notDone = [...urgent, ...notUrgent].filter((todoItem) => !todoItem.isDone);
+			$todos = [...notDone, ...done];
 		}}
 	>
 		<span
-			class="active:bg-success hover:bg-green-100 hover:text-neutral active:text-white mobileOnly:hover:bg-white mobileOnly:hover:text-neutral mobileOnly:focus:bg-green-300 mobileOnly:active:bg-success mobileOnly:active:text-white"
+			class="active:bg-info hover:bg-blue-100 hover:text-neutral active:text-white mobileOnly:hover:bg-white mobileOnly:hover:text-neutral mobileOnly:focus:bg-blue-300 mobileOnly:active:bg-info mobileOnly:active:text-white"
 		>
 			<svg
-				class="w-5 h-5"
+				class="w-6 h-6"
 				fill="none"
 				stroke="currentColor"
 				viewBox="0 0 24 24"
@@ -25,7 +32,7 @@
 					stroke-linecap="round"
 					stroke-linejoin="round"
 					stroke-width="2"
-					d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+					d="M3 4h13M3 8h9m-9 4h6m4 0l4-4m0 0l4 4m-4-4v12"
 				/></svg
 			>
 		</span>
@@ -45,37 +52,6 @@
 					stroke-linejoin="round"
 					stroke-width="2"
 					d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-				/></svg
-			>
-		</span>
-	</li>
-	<li
-		on:click={() => {
-			const hard = $todos.filter((todoItem) => todoItem.isHard);
-			const notHard = $todos.filter((todoItem) => !todoItem.isHard);
-			const important = [...hard, ...notHard].filter((todoItem) => todoItem.isImportant);
-			const notImportant = [...hard, ...notHard].filter((todoItem) => !todoItem.isImportant);
-			const urgent = [...important, ...notImportant].filter((todoItem) => todoItem.isUrgent);
-			const notUrgent = [...important, ...notImportant].filter((todoItem) => !todoItem.isUrgent);
-			const done = [...urgent, ...notUrgent].filter((todoItem) => todoItem.isDone);
-			const notDone = [...urgent, ...notUrgent].filter((todoItem) => !todoItem.isDone);
-			$todos = [...notDone, ...done];
-		}}
-	>
-		<span
-			class="active:bg-info hover:bg-blue-100 hover:text-neutral active:text-white mobileOnly:hover:bg-white mobileOnly:hover:text-neutral mobileOnly:focus:bg-blue-300 mobileOnly:active:bg-info mobileOnly:active:text-white"
-		>
-			<svg
-				class="w-5 h-5"
-				fill="none"
-				stroke="currentColor"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-				><path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M3 4h13M3 8h9m-9 4h9m5-4v12m0 0l-4-4m4 4l4-4"
 				/></svg
 			>
 		</span>
