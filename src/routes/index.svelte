4310e2157d02ec4489d84b904f011eb8c8af0aec
Subhasish Das <subhasish@doc32.in>
2022-02-24 22:39:50 +0530

    down fucntion okay

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,6 +30,19 @@
 		}, 2000);
 		return () => clearInterval(placeholderInterval);
 	});
+
+	const moveTodo = (todo, direction) => {
+		console.log('direction', todo);
+		if (direction === 'down' && $todos.indexOf(todo) + 1 !== $todos.length) {
+			const nextValue = $todos[$todos.indexOf(todo) + 1];
+			$todos[$todos.indexOf(todo) + 1] = todo;
+			$todos[$todos.indexOf(todo)] = nextValue;
+		} else {
+			const prevValue = $todos[$todos.indexOf(todo) - 1];
+			$todos[$todos.indexOf(todo) - 1] = todo;
+			$todos[$todos.indexOf(todo)] = prevValue;
+		}
+	};
 </script>
 
 <div class="mt-6 mb-28 px-6">
@@ -107,45 +120,42 @@
 								type="checkbox"
 								checked={todo.isDone}
 								class="checkbox checkbox-primary block w-6 h-6 flex-shrink-0"
-								on:change={() => {
-									const index = $todos.indexOf(
-										$todos.filter((todoItem) => todoItem.id === todo.id)[0]
-									);
-									$todos[index].isDone = !todo.isDone;
-								}}
+								on:change={() => (todo.isDone = !todo.isDone)}
 							/>
 							<span class={`label-text ${todo.isDone && 'line-through'}`}>{todo.text}</span>
 						</label>
 					</div>
 					<div class="justify-end card-actions mt-4">
 						<div class="btn-group mr-2">
-							<div class="btn btn-sm btn-secondary px-1">
+							<div
+								class="btn btn-sm btn-secondary px-1 mobileOnly:hover:bg-secondary mobileOnly:hover:border-secondary"
+								on:click={() => moveTodo(todo, 'up')}
+							>
 								<svg
 									class="w-6 h-6"
-									fill="none"
-									stroke="currentColor"
-									viewBox="0 0 24 24"
+									fill="currentColor"
+									viewBox="0 0 20 20"
 									xmlns="http://www.w3.org/2000/svg"
 									><path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M15 19l-7-7 7-7"
+										fill-rule="evenodd"
+										d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 0l-3 3a1 1 0 001.414 1.414L9 9.414V13a1 1 0 102 0V9.414l1.293 1.293a1 1 0 001.414-1.414z"
+										clip-rule="evenodd"
 									/></svg
 								>
 							</div>
-							<div class="btn btn-sm btn-secondary px-1">
+							<div
+								class="btn btn-sm btn-secondary px-1 mobileOnly:hover:bg-secondary mobileOnly:hover:border-secondary"
+								on:click={() => moveTodo(todo, 'down')}
+							>
 								<svg
 									class="w-6 h-6"
-									fill="none"
-									stroke="currentColor"
-									viewBox="0 0 24 24"
+									fill="currentColor"
+									viewBox="0 0 20 20"
 									xmlns="http://www.w3.org/2000/svg"
 									><path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M9 5l7 7-7 7"
+										fill-rule="evenodd"
+										d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v3.586L7.707 9.293a1 1 0 00-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 10.586V7z"
+										clip-rule="evenodd"
 									/></svg
 								>
 							</div>
