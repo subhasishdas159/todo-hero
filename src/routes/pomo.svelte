c442261231b95a6c3f7d8ae70212f7206855a7a8
Subhasish Das <subhasish@doc32.in>
2022-02-26 15:37:06 +0530

    bug fixes

--- src/routes/pomo.svelte
+++ src/routes/pomo.svelte
@@ -13,8 +13,7 @@
 	if (browser) {
 		if (!$page.url.searchParams.has('id')) {
 			noIdGoBack = true;
-		}
-		if (
+		} else if (
 			!$todos.filter((todoItem) => String(todoItem.id) === $page.url.searchParams.get('id')).length
 		) {
 			wrongIdGoBack = true;
@@ -22,7 +21,21 @@
 	}
 </script>
 
-pomo screen
+<div class="mx-auto h-6 w-6 mt-8" on:click={() => goto('/')}>
+	<svg
+		class="w-full h-full"
+		fill="none"
+		stroke="currentColor"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+		><path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			d="M10 19l-7-7m0 0l7-7m-7 7h18"
+		/></svg
+	>
+</div>
 
 <Modal
 	bind:show={noIdGoBack}
